13e3e093e9c00646277665cf73b637fbe01bed0a
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-04-06 00:15:14 -0400

    Add files via upload

---
+++ GraphicCalculator/src/Calculator.java
@@ -0,0 +1,22 @@
+/*
+ * This is the class where a calculator can be created to perform a variety of 
+ * mathematical operations.
+ */
+
+/**
+ *
+ * @author Syed Yousuf
+ */
+public class Calculator 
+{
+    private String currentDisplay;
+    
+    
+    /*Defualt Constructor*/
+    public Calculator()
+    {
+        // Set the current display to an empty string
+        currentDisplay = "";
+    }
+    
+}
